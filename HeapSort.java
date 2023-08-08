1084c763ca632b1adae354f791af630fe81f2a03
v1ad-mir <gardfil87@mail.ru>
2023-08-08 18:41:29 +0300

    commit

---
+++ HeapSort.java
@@ -0,0 +1,73 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+//Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+public class HeapSort {
+
+
+    public static void main(String args[]) {
+        int arr[] = new int[10];
+//        int n = arr.length;
+        for (int i = 0; i < 10; i++) {
+            arr[i]= ThreadLocalRandom.current().nextInt(0, 100);
+        }
+        System.out.println("Заданный массив массив");
+        printArray(arr);
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Отсортированный массив");
+        printArray(arr);
+    }
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+
+            heapify(arr, i, 0);
+        }
+    }
+
+
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+
+            heapify(arr, n, largest);
+        }
+    }
+
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+
+
+}
